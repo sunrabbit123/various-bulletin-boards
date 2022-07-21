3020af4f1fbcf771b839979ccfee5a86278ca6a7
sunrabbit123 <qudwls185@naver.com>
2022-07-22 01:54:12 +0900

    [UPDATE] repository

---
+++ ts/koa/src/repository/board.repo.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from "@prisma/client";
+
+interface BoardBase {
+  title: string;
+  content: string;
+  tag: string;
+}
+const PAGE_TAKEN = 15;
+const prisma = new PrismaClient();
+
+export const getB: Function = async (idx: number) =>
+  prisma.board.findFirst({ where: { idx } });
+
+export const getBList: Function = async ({
+  page,
+  direction,
+  sortBy,
+}: {
+  page: number;
+  direction: "asc" | "desc";
+  sortBy: "createdAt";
+}) =>
+  prisma.board.findMany({
+    take: PAGE_TAKEN,
+    orderBy: { [sortBy]: direction },
+    skip: (page - 1) * PAGE_TAKEN,
+  });
+
+export const insertB: Function = async (data: BoardBase) =>
+  prisma.board.create({ data });
+
+export const patchB: Function = async (idx: number, data: Partial<BoardBase>) =>
+  prisma.board.update({ data, where: { idx } });
+
+export const deleteB: Function = async (idx: number) =>
+  prisma.board.delete({ where: { idx } });
