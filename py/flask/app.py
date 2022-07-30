54dc5b3b89a484cedd11664dc7b51206c21028c7
sunrabbit123 <qudwls185@naver.com>
2022-07-30 13:43:54 +0900

    [ADD] set flask default setting

---
+++ py/flask/app.py
@@ -0,0 +1,32 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config["JSON_AS_ASCII"] = False
+
+def generate_response(data, code : str = "DP000", status : str = "success"):
+    return {"status": status, "code": code, "data": data}
+
+@app.route("/board/:idx")
+def get_board():
+    return generate_response()
+
+@app.route("/board")
+def get_board_list():
+    return generate_response()
+
+@app.route("/board")
+def create_board():
+    return generate_response()
+
+@app.route("/board/:idx")
+def patch_board():
+    return generate_response()
+
+@app.route("/board/:idx")
+def delete_board():
+    return generate_response()
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
